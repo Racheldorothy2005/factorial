cdba8ce2a89e95e887ae11ea4e9a8ad5ea592e00
Racheldorothy2005 <gracheldorothy@gmail.com>
2025-04-23 20:10:42 +0530

    using recursion

--- sample.py
+++ sample.py
@@ -1,7 +1,6 @@
 def factorial(n):
-    result =1
-    for i in range(2,n+1):
-          result*=i
-    return result
-
-print(factorial(5))
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))        
